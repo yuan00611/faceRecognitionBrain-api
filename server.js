f7f201cdf28f80912545b8631252055a9d527b6e
陳德元 <r06521608@ntu.edu.tw>
2020-06-02 22:29:57 -0500

    adding port

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 //     // res = false
 // });
 
- app.listen(3000, () => {
+ app.listen(process.env.PORT || 3000, () => {
  	console.log('app is running on port 3000');
  })
 

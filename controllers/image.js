9d0dfc829fc3a6d129b578efbeb33860c1c5814c
陳德元 <r06521608@ntu.edu.tw>
2020-06-02 22:06:39 -0500

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '6afd46ecabdd480e8caa21cbdaf85f86'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+      .predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
+      .then(data => {
+      	res.json(data);
+      })
+      .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then( entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+}
